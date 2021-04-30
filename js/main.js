7ebf74deb4b39437474f35a03424167f5e99b852
Zac Heisey <zheisey@gmail.com>
2021-04-30 13:49:08 -0400

    update netlify functions directory path

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-  await (await fetch('./netlify/lambda/getusers')).json();
+  await (await fetch('/.netlify/functions/getusers')).json();
 
 fetchUsers().then(data => {
 
